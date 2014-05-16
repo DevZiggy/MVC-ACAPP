f28a1fde313a07e55f852b68630660839318a9d9
DevZiggy <github@checkthis2.nl>
2014-05-16 22:07:41 +0200

    Basics

---
+++ content/Controllers/AcappBaseController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using System.Web.Mvc;
+using System.Web.Script.Serialization;
+using com.checkthis2.acapp;
+
+namespace ACAPP.Controllers
+{
+    public class AcappBaseController : Controller
+    {
+        /// <summary>
+        /// generate a view and return as string
+        /// </summary>
+        /// <remarks>
+        /// 2014-05-16: Created
+        /// </remarks>
+        public string ViewToString(string viewName, object model)
+        {
+            ViewData.Model = model;
+            using (var sw = new StringWriter())
+            {
+                var viewResult = ViewEngines.Engines.FindPartialView(ControllerContext, viewName);
+                var viewContext = new ViewContext(ControllerContext, viewResult.View, ViewData, TempData, sw);
+                viewResult.View.Render(viewContext, sw);
+                viewResult.ViewEngine.ReleaseView(ControllerContext, viewResult.View);
+                return sw.GetStringBuilder().ToString();
+            }
+        }
+
+        /// <summary>
+        /// Generate a JSON result.
+        /// </summary>
+        /// <remark>
+        /// This function allowes for larger responds. The standard response would generate an error on very large forms
+        /// </remark>
+        /// <remarks>
+        /// 2013-05-16: Created
+        /// </remarks>
+        public ContentResult JsonSerialized(AcappBase result)
+        {
+            if (result == null)
+                return null;
+
+            //result.UpdateNotification();
+
+            var serializer = new JavaScriptSerializer { MaxJsonLength = Int32.MaxValue };
+            return new ContentResult
+            {
+                Content = serializer.Serialize(result),
+                ContentType = "application/json"
+            };
+        }
+
+    }
+}
--- content/Scripts/acapp.js
+++ content/Scripts/acapp.js
@@ -1,1 +1,51 @@
-alert(1);
+(function(_w, _d, $) {
+    /* STORE REFERENCES TO PREVENT EXTERNAL INFLUENCES
+       _w = window
+       _d = document
+       _s = this
+    */
+    var _s = this;
+
+    /* DEFAULT SETTINGS */
+    var settings = {        
+        homepage: '/acapp/home/'
+    };
+
+    /* CONTAINERS */
+    var $application = null;
+
+    /* MAIN FUNCTION TO ADD BIND */
+    $.fn.Acapp = function(method) {
+        if (typeof(_s[method]) == "function") {
+            return _s[method].apply(this, Array.prototype.slice.call(arguments, 1));
+        } else if (typeof method === 'object' || !method) {
+            return _s.init.apply(this, arguments);
+        } else {
+            $.error('Method ' + method + ' does not exist on jQuery');
+            return false;
+        }
+    };
+
+    /* STARTUP */
+
+    /// <summary>
+    /// This is the base method that is called. It will load the basic settings
+    /// </summary>
+    /// <remarks>
+    /// 2014-05-16: Created
+    /// </remarks>
+    init = function(options) {
+        //make sure the site loads on base url
+        if (window.location.pathname != '/') {
+            window.location.href = '/';
+        }
+
+        //store the container
+        $application = $(this);
+        
+        //load user settings
+        settings = $.extend(settings, options);
+    }
+    
+})(window, document, jQuery);
+
